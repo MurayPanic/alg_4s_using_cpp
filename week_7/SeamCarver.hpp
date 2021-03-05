3165939520fc73ad8853e3aaec631f5f1c85eeb8
MurayPanic <lbwkabuto@gmail.com>
2021-03-05 14:20:24 +0800

    Successfully calculate the seam

--- week_7/SeamCarver.hpp
+++ week_7/SeamCarver.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <climits>
 
 #include "Picture.hpp"
 
@@ -24,16 +25,18 @@ class SeamCarver{
             int width();
             int height();
             double energy(int, int);
-            std::vector<int> findHorizontalSeam();
-            std::vector<int> findVerticalSeam();
+            std::vector<std::pair<int, int> > findHorizontalSeam();
+            std::vector<std::pair<int, int> > findVerticalSeam();
             void removeHorizontalSeam( std::vector<int>);
             void removeVerticalSeam( std::vector<int> );
 
  	private:
 	    Picture pic_data;
 	    cv::Mat energy_map;
-	    void gen_energy_map();
-	    double cal_pixel_energy(int, int);
+	    void genEnergyMap();
+	    double calPixelEnergy(int, int);
+		bool validateCoordinate(int, int);
+		
             
 
 };
@@ -42,10 +45,10 @@ class SeamCarver{
 SeamCarver::SeamCarver(){}
 
 SeamCarver::SeamCarver(Picture PIC_ins): pic_data(PIC_ins){
-	this->gen_energy_map();
+	this->genEnergyMap();
 }
 
-SeamCarver::~SeamCarver(){}
+SeamCarver::~SeamCarver(){ }
 
 //Member function
 Picture SeamCarver::picture(){
@@ -68,12 +71,12 @@ double SeamCarver::energy(int x, int y){
 
 }
 
-void SeamCarver::gen_energy_map(){
+void SeamCarver::genEnergyMap(){
 	this->energy_map= cv::Mat::zeros(this->pic_data.height(),this->pic_data.width(), CV_64F);
 
 	for(int row{0}; row< this->pic_data.height(); ++row){
 		for(int col{0}; col< this->pic_data.width(); ++col){
-			double tmp_energy= this->cal_pixel_energy(row, col);
+			double tmp_energy= this->calPixelEnergy(row, col);
 			this->energy_map.at<double>(row, col) = tmp_energy; 
 		
 		}
@@ -83,7 +86,7 @@ void SeamCarver::gen_energy_map(){
 
 }
 
-double SeamCarver::cal_pixel_energy(int row, int col){
+double SeamCarver::calPixelEnergy(int row, int col){
 	double total_energy{0};
 
 	cv::Vec3b pixel_center = this->pic_data.get(col, row);
@@ -108,4 +111,159 @@ double SeamCarver::cal_pixel_energy(int row, int col){
 	}
 
 	return total_energy;
+}
+
+std::vector<std::pair<int, int> > SeamCarver::findVerticalSeam(){
+
+		std::vector< std::pair<int, int> > vertical_seam{}; 
+		
+		cv::Mat distTo= cv::Mat::zeros(this->pic_data.height(), this->pic_data.width(), CV_64FC1);
+		cv::Mat pixelTo= cv::Mat::zeros(this->pic_data.height(), this->pic_data.width(), CV_32S );
+
+		for(int row{0}; row< this->pic_data.height(); ++row){
+			
+			for(int col={0}; col< this->pic_data.width(); ++col){
+				//std::cout<<"Now handle pixel: ("<<row<<","<<col<<")"<<std::endl;
+				
+				if(row==0){
+					distTo.at<double>(row,col) =  this->energy(col, row);
+					pixelTo.at<int>(row, col) = 0;
+					continue;
+				}
+
+				
+				distTo.at<double>(row, col) = this->energy(col, row);
+				//std::cout<<"The wrighten energy is: "<<distTo.at<double>(row, col)<<std::endl;
+
+				//validate the pixel corodinates for previous points (x-1, y-1) (x-1, y-1),(x+1, y-1)
+				//find out the shortest length from previous point
+
+				double dist_topleft= DBL_MAX;  //distance of previous point (row-1, col-1)
+				double dist_top = DBL_MAX;	//distance of previous point (row-1, col)
+				double dist_topright = DBL_MAX; //distance of previous point (row-1, col+1)
+				
+				if(this->validateCoordinate(row-1, col-1 )){
+					dist_topleft= distTo.at<double>( row-1,col-1);
+					//std::cout<<"find valid topleft: "<<dist_topleft<<std::endl;
+					
+				}
+
+				if(this->validateCoordinate(row-1, col )){
+					dist_top= distTo.at<double>(row-1, col);
+					//std::cout<<"find valid top:"<< dist_top<<std::endl;
+				}
+
+				if(this->validateCoordinate(row-1, col+1 )){
+					dist_topright= distTo.at<double>(row-1, col+1);
+					//std::cout<<"find valid topright"<< dist_topright<<std::endl;
+				}
+				
+				std::vector<double> dist_vec{dist_topleft, dist_top, dist_topright};
+				
+
+				auto tmp_distTo_iter= std::min_element(dist_vec.begin(), dist_vec.end());
+				
+
+				
+				/*
+				std::cout<<"The dist vec is:";
+				for(auto item: dist_vec){
+					std::cout<<item<< " ";
+				}
+				std::cout<<std::endl;
+				
+				
+				std::cout<<"The minimum dist is"<< (*tmp_distTo_iter)<<std::endl;
+				*/
+
+				int tmp_pixelTo = std::distance(dist_vec.begin(), tmp_distTo_iter);
+				//handle the situation that equal energy exists
+				if(tmp_pixelTo==0){ 
+						tmp_pixelTo= dist_vec[tmp_pixelTo] == dist_vec[tmp_pixelTo+1] ? tmp_pixelTo+1 : tmp_pixelTo;
+					}
+				
+				if(tmp_pixelTo==2){ 
+						tmp_pixelTo= dist_vec[tmp_pixelTo] == dist_vec[tmp_pixelTo-1] ? tmp_pixelTo-1 : tmp_pixelTo;
+				}
+				
+				
+
+				distTo.at<double>(row,col) +=  (*tmp_distTo_iter);
+				
+				//std::cout<<"The final energy is: "<<distTo.at<double>(row, col)<<std::endl;
+
+				
+				
+				
+				pixelTo.at<int>(row, col) = tmp_pixelTo==0  ?  -1  :  (tmp_pixelTo==1 ? 0 : 1 );
+
+				//std::cout<<"The Pixel to is is: "<<pixelTo.at<int>(row, col)<<std::endl;
+				
+			}
+		}
+		
+		for(int row{0}; row< this->pic_data.height(); ++row){
+			for(int col{0}; col< this->pic_data.width(); ++col){
+				std::cout<<" Target pixel: ("<<row<<","<<col<<")"<<":"
+						<<"distTo: "<<distTo.at<double>(row,col)
+						<<" PixelTo: "<<pixelTo.at<int>(row, col) 
+						<<" Energy: "<< this->energy(col, row)
+				<<std::endl;
+				
+
+			}
+			
+		}
+		
+
+
+		//finde minimum energy
+		
+		std::vector<double> last_row_in_distTo{};
+		int last_row_index = this->height()-1;
+
+		for (int col{0}; col< this->width(); ++col){
+				last_row_in_distTo.push_back( distTo.at<double>(last_row_index, col) );
+		}
+		
+		auto min_energy_pos= std::min_element(last_row_in_distTo.begin(), last_row_in_distTo.end() );
+
+		auto end_point_col= std::distance(last_row_in_distTo.begin(), min_energy_pos);
+
+		//Find the seam
+		int tmp_col = end_point_col;
+		int tmp_row = last_row_index;
+		std::cout<<"the endpoint of the seam: "<<tmp_row<<","<<tmp_col<<std::endl;
+
+		for(tmp_row; tmp_row>=0; --tmp_row){
+			std::pair<int, int> pixel_coor(tmp_row, tmp_col);
+			vertical_seam.push_back(pixel_coor);
+			tmp_col += pixelTo.at<int>(tmp_row,tmp_col);
+		}
+
+
+
+
+
+		std::cout<<"Process complete"<<std::endl;
+
+		
+		
+
+		//Obtain the vertical seam from the distTo img
+		
+		return vertical_seam;
+
+
+}
+
+
+bool SeamCarver::validateCoordinate(int row, int col){
+
+	if(  0<=row && row< this->height() && 0<=col  && col < this->width()){
+		return true;
+	}else{
+		return false;
+	}
+
 }
--- week_7/test_seamcarver.cpp
+++ week_7/test_seamcarver.cpp
@@ -20,6 +20,7 @@ TEST(SeamCarver_constructor, case_2){
 
 }
 
+
 TEST(SeamCarver_picture, case_1){
 	Picture PIC_ins(400,300);
 	SeamCarver SC_ins(PIC_ins);
@@ -46,10 +47,10 @@ TEST(SeamCarver_cal_pixel_energy, cast_1){
 	Picture PIC_ins(img_file_name);
 	SeamCarver SC_ins(PIC_ins);
 
-	double energy_1 = SC_ins.cal_pixel_energy(0,0);
+	double energy_1 = SC_ins.calPixelEnergy(0,0);
 	GTEST_COUT << "The energy at (0,0) is " << energy_1 <<std::endl;
 
-	double energy_2 = SC_ins.cal_pixel_energy(80,60);
+	double energy_2 = SC_ins.calPixelEnergy(80,60);
 	GTEST_COUT << "The energy at (80,60) is " << energy_2 <<std::endl;
 
 }
@@ -95,7 +96,7 @@ TEST(SeamCarver_cal_pixel_energy, cast_2){
 	GTEST_COUT<< "Expect total energy: " << expect_total_energy<<std::endl;
 				
 
-	double energy_2 = SC_ins.cal_pixel_energy(80,60);
+	double energy_2 = SC_ins.calPixelEnergy(80,60);
 	GTEST_COUT << "The energy at (80,60) is " << energy_2 <<std::endl;
 
 	EXPECT_EQ(expect_total_energy, energy_2);
@@ -147,24 +148,250 @@ TEST(SeamCarver_show_energy_map, case_1){
 	cv::waitKey(0);
 }
 
-/*
-TEST(SeamCarver_show_energy_map, case_2){
 
-	Picture PIC_ins(300,400);
-	SeamCarver SC_ins(PIC_ins);
+TEST(SeamCarver_findVerticalSeam, case_1){
+	std::string img_file_name="surfing.png";
+	Picture PIC_ins(img_file_name);
+	SeamCarver SC_ins(PIC_ins);	
 
-	auto energy_map = SC_ins.energy_map;
-	
-	for(auto iter =energy_map.begin<float>(); iter!=energy_map.end<float>(); ++iter){
-		double energy_val = *iter;
-		*iter = (int)energy_val % 255;
-		GTEST_COUT<<"The revised energy value is: "<< energy_val<<std::endl;
+	auto vertical_seam = SC_ins.findVerticalSeam();
+	GTEST_COUT<< "Now print the seam: "<<std::endl;
+
+	for(auto item : vertical_seam){
+		std::cout<< GTEST_LINE_HEADER 
+				<< "("<<item.first <<","<<item.second <<")"
+				<<std::endl;
 	}
 
-	//cv::imshow("Energy Map", energy_map);
-	//cv::waitKey(0);
+	GTEST_COUT<< "Now show the seam on energy map: "<<std::endl;
+	auto energy_map = SC_ins.energy_map;
+	cv::Mat energy_img;
+	energy_map.convertTo(energy_img, CV_8UC3);
+	
+	cv::imshow("Energy image", energy_img);
+	cv::waitKey(0);
+
+	for(auto item : vertical_seam){
+		GTEST_COUT<<"Now color: ("<<item.first<<","<<item.second<<")"<<std::endl;
+		auto pixel =energy_img.at<cv::Vec3b>(item.first, item.second);
+		GTEST_COUT<<"The current pixel value is "<< (double)pixel[0]<<","
+												<< (double)pixel[1]<<","
+												<< (double)pixel[2]<<"," <<std::endl;
+		energy_img.at<cv::Vec3b>(item.first, item.second)=cv::Vec3b(0,0, 255);
+
+		pixel =energy_img.at<cv::Vec3b>(item.second, item.first);
+		GTEST_COUT<<"The modified pixel value is "<< (double)pixel[0]<<","
+												<< (double)pixel[1]<<","
+												<< (double)pixel[2]<<"," <<std::endl;
+
+	}
+	cv::imshow("Energy image with seam", energy_img);
+	cv::waitKey(0);
+
+	
+
+
 }
-*/
+
+TEST(SeamCarver_visualVerticalSeam, case_1){
+	std::string img_file_name="6x5.png";
+	Picture PIC_ins(img_file_name);
+	SeamCarver SC_ins(PIC_ins);	
+
+	auto vertical_seam = SC_ins.findVerticalSeam();
+	GTEST_COUT<< "Now print the seam: "<<std::endl;
+
+	for(auto item : vertical_seam){
+		std::cout<< GTEST_LINE_HEADER 
+				<< "("<<item.first <<","<<item.second <<")"
+				<<std::endl;
+	}
+
+	GTEST_COUT<< "Now show the seam on energy map: "<<std::endl;
+	auto energy_map = SC_ins.energy_map;
+	cv::Mat energy_img;
+	energy_map.convertTo(energy_img, CV_8UC3);
+	
+	GTEST_COUT<<"Energy image size: "<< energy_img.dims<<std::endl;
+	GTEST_COUT<<"Energy image depth: "<< energy_img.depth()<<std::endl;
+	GTEST_COUT<<"Energy image total element: "<< energy_img.total()<<std::endl;
+	cv::imshow("Energy image", energy_img);
+	cv::waitKey(0);
+
+	GTEST_COUT<<"The original energy image is"<<std::endl;
+
+	for(int row{0}; row<energy_img.rows;++row){
+		std::cout<< GTEST_LINE_HEADER;
+		for (int col{0}; col<energy_img.cols; ++col){
+			auto pixel =energy_img.at<cv::Vec3b>(row,col);
+			std::cout<< "("<< (double)pixel[0]<<","
+												<< (double)pixel[1]<<","
+												<< (double)pixel[2]<<"," 
+												<<") ,";
+		}
+		std::cout<<std::endl;
+	}
+
+	for(auto item : vertical_seam){
+		GTEST_COUT<<"Now color: ("<<item.first<<","<<item.second<<")"<<std::endl;
+		auto pixel =energy_img.at<cv::Vec3b>(item.first, item.second);
+		GTEST_COUT<<"The current pixel value is "<< (double)pixel[0]<<","
+												<< (double)pixel[1]<<","
+												<< (double)pixel[2]<<"," <<std::endl;
+		
+		/*
+		energy_img.at<cv::Vec3b>(item.first, item.second)[0]=45;
+		energy_img.at<cv::Vec3b>(item.first, item.second)[1]=45;
+		energy_img.at<cv::Vec3b>(item.first, item.second)[2]=45;
+		*/
+	energy_img.at<cv::Vec3b>(item.first, item.second)= cv::Vec3b(45,45,45);
+
+		pixel =energy_img.at<cv::Vec3b>(item.first, item.second);
+		GTEST_COUT<<"The modified pixel value is "<< (double)pixel[0]<<","
+												<< (double)pixel[1]<<","
+												<< (double)pixel[2]<<"," <<std::endl;
+
+		GTEST_COUT<<"-------------------------------------"<<std::endl;
+
+		GTEST_COUT<<"Current revised energy image is"<<std::endl;
+
+		for(int row{0}; row<energy_img.rows;++row){
+			std::cout<< GTEST_LINE_HEADER;
+			for (int col{0}; col<energy_img.cols; ++col){
+				auto pixel =energy_img.at<cv::Vec3b>(row,col);
+				std::cout<< "("<< (double)pixel[0]<<","
+													<< (double)pixel[1]<<","
+													<< (double)pixel[2]<<"," 
+													<<") ,";
+			}
+			std::cout<<std::endl;
+		}
+
+		GTEST_COUT<<"++++++++++++++++++++++++++++++++++++++++="<<std::endl;
+
+	}
+
+
+	GTEST_COUT<<"The revised energy image is"<<std::endl;
+
+	for(int row{0}; row<energy_img.rows;++row){
+		std::cout<< GTEST_LINE_HEADER;
+		for (int col{0}; col<energy_img.cols; ++col){
+			auto pixel =energy_img.at<cv::Vec3b>(row,col);
+			std::cout<< "("<< (double)pixel[0]<<","
+												<< (double)pixel[1]<<","
+												<< (double)pixel[2]<<"," 
+												<<") ,";
+		}
+		std::cout<<std::endl;
+	}
+	cv::imshow("Energy image with seam", energy_img);
+	cv::waitKey(0);
+
+
+
+	
+
+
+}
+
+TEST(SeamCarver_showVerticalSeam, case_1){
+	std::string img_file_name="6x5.png";
+	Picture PIC_ins(img_file_name);
+	SeamCarver SC_ins(PIC_ins);	
+
+	auto vertical_seam = SC_ins.findVerticalSeam();
+	GTEST_COUT<< "Now print the seam: "<<std::endl;
+
+	for(auto item : vertical_seam){
+		std::cout<< GTEST_LINE_HEADER 
+				<< "("<<item.first <<","<<item.second <<")"
+				<<std::endl;
+	}
+
+	GTEST_COUT<< "Now show the seam on energy map: "<<std::endl;
+	auto energy_map = SC_ins.energy_map;
+	cv::Mat energy_img;
+	energy_map.convertTo(energy_img, CV_8UC1);
+	
+	
+	cv::imshow("Energy image", energy_img);
+	cv::waitKey(0);
+	
+	GTEST_COUT<<"The original energy image is"<<std::endl;
+
+	for(int row{0}; row<energy_img.rows;++row){
+		std::cout<< GTEST_LINE_HEADER;
+		for (int col{0}; col<energy_img.cols; ++col){
+			auto pixel =energy_img.at<uchar>(row,col);
+			std::cout<< "("<< (double)pixel <<") ,";
+		}
+		std::cout<<std::endl;
+	}
+
+	for(auto item : vertical_seam){
+		GTEST_COUT<<"Now color: ("<<item.first<<","<<item.second<<")"<<std::endl;
+		auto pixel =energy_img.at<uchar>(item.first, item.second);
+		GTEST_COUT<<"The current pixel value is "<< (double)pixel <<std::endl;
+		
+		
+		energy_img.at<uchar>(item.first, item.second)=45;
+
+
+		pixel =energy_img.at<uchar>(item.first, item.second);
+		GTEST_COUT<<"The modified pixel value is "<< (double)pixel <<std::endl;
+
+		GTEST_COUT<<"-------------------------------------"<<std::endl;
+
+		GTEST_COUT<<"Current revised energy image is"<<std::endl;
+
+		for(int row{0}; row<energy_img.rows;++row){
+			std::cout<< GTEST_LINE_HEADER;
+			for (int col{0}; col<energy_img.cols; ++col){
+				auto pixel =energy_img.at<uchar>(row,col);
+				std::cout<< "("<< (double)pixel<<") ,";
+			}
+			std::cout<<std::endl;
+		}
+
+		GTEST_COUT<<"++++++++++++++++++++++++++++++++++++++++="<<std::endl;
+
+	}
+
+
+	GTEST_COUT<<"The revised energy image is"<<std::endl;
+
+	for(int row{0}; row<energy_img.rows;++row){
+		std::cout<< GTEST_LINE_HEADER;
+		for (int col{0}; col<energy_img.cols; ++col){
+			auto pixel =energy_img.at<uchar>(row,col);
+			std::cout<< "("<< (double)pixel<<") ,";
+		}
+		std::cout<<std::endl;
+	}
+	cv::imshow("Energy image with seam", energy_img);
+	cv::waitKey(0);
+
+	
+
+	
+
+
+}
+
+
+TEST(SeamCarver_findVerticalSeam_2, case_2){
+	
+	Picture PIC_ins(10,20);
+	SeamCarver SC_ins(PIC_ins);	
+
+	SC_ins.findVerticalSeam();
+	
+
+
+}
+
+
 int main(int argc, char* argv[]){
 
     std::cout<<"This script is to test the SeamCarver head file"
